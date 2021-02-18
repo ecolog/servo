c929cb2b46c92a7eb0dab5acf69f909440ac63a4
Josh Matthews <josh@joshmatthews.net>
2021-02-18 11:49:16 -0500

    tidy fixes

--- etc/ci/generate_workflow.py
+++ etc/ci/generate_workflow.py
@@ -1,12 +1,24 @@
+# Copyright 2021 The Servo Project Developers. See the COPYRIGHT
+# file at the top-level directory of this distribution.
+#
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+# <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+# option. This file may not be copied, modified, or distributed
+# except according to those terms.
+
 import os.path
 import sys
 
 BASE = os.path.dirname(__file__.replace('\\', '/'))
-sys.path.insert(0, os.path.join(BASE, "..", "..", "components", "style", "properties", "Mako-1.1.2-py2.py3-none-any.whl"))
+sys.path.insert(0, os.path.join(
+    BASE, "..", "..", "components", "style", "properties", "Mako-1.1.2-py2.py3-none-any.whl"
+))
 
-from mako import exceptions
-from mako.lookup import TemplateLookup
-from mako.template import Template
+from mako import exceptions  # noqa
+from mako.lookup import TemplateLookup  # noqa
+from mako.template import Template  # noqa
+
 
 def abort(message):
     print(message, file=sys.stderr)
@@ -38,6 +50,7 @@ def main():
             'workflow.mako',
             total_chunks=20,
         ))
+
 
 if __name__ == "__main__":
     main()
